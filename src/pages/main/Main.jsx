aa1cb01edcc4502759e6d52ce6dcd144d021d007
RJ Tony <ray.developper@gmail.com>
2021-08-21 19:25:13 +0100

    Implementando icone de dark mode

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -3,7 +3,18 @@ import './Main.css';
 
 function Main(){
   return(
-    <div className="main-container">Simpe Divvv</div>
+    <div className="main-container">
+      <div className="mode-container">
+        <div className="mode-icone dark"><i className="dark-icone"/></div>
+        <div className="mode-icone light"><i className="light-icone"/></div>
+      </div>
+      <div className="up">
+
+      </div>
+      <div className="down">
+        
+      </div>
+    </div>
   );
 }
 
