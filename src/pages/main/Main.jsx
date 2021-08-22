d74c49a869fcea3f5c7a3d139adf69d565539ce9
RJ Tony <ray.developper@gmail.com>
2021-08-22 23:36:25 +0100

    UI Terminada

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -9,10 +9,33 @@ function Main(){
         <div className="mode-icone dark"><i className="dark-icone far fa-moon"/></div>
       </div>
       <div className="up">
-
+        <p className="operation">10 &times; 10 &times; 10</p>
+        <h1 className="result">1000</h1>
       </div>
       <div className="down">
-        
+        <div className="teclado">
+          <button class="action" id="clear">C</button>
+          <button class="action" id="backspace">CE</button>
+          <button class="operator" id="percent">%</button>
+          <button class="operator" id="divide">&#247;</button>
+          <button class="number" id="7">7</button>
+          <button class="number" id="8">8</button>
+          <button class="number" id="9">9</button>
+          <button class="operator" id="multiply">&times;</button>
+          <button class="number" id="4">4</button>
+          <button class="number" id="5">5</button>
+          <button class="number" id="6">6</button>
+          <button class="operator" id="menus">-</button>
+          <button class="number" id="1">1</button>
+          <button class="number" id="2">2</button>
+          <button class="number" id="3">3</button>
+          <button class="operator" id="sum">+</button>
+          <button class="operator" id="parentes">( )</button>
+          <button class="number" id="0">0</button>
+          <button class="number" id="coma">,</button>
+          <button class="operator" id="equal">=</button>
+        </div>
+        <div className="simple-line"></div>
       </div>
     </div>
   );
